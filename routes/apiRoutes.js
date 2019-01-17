77fdec137dc7a39bdb74caf2d3bfad5eaa2cca83
Skarrg <orestes.moiran@gmail.com>
2019-01-16 20:22:08 -0800

    Trying to make travis happy

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -28,7 +28,9 @@ module.exports = function(app) {
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.json(dbExample);
     });
   });
