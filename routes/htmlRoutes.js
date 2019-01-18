96f4d7b10c8dbcc8b5582e232ccd7a0ab361879c
cindy <42073246+cxlhyphen@users.noreply.github.com>
2019-01-18 01:37:09 -0500

    finished basic layout, 404 pg, and added new snippet page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -11,11 +11,19 @@ module.exports = function(app) {
     });
   });
 
+  app.get("/newsnippet", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("newsnippet", {
+        examples: dbExamples
+      });
+    });
+  });
+
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
       res.render("example", {
-        example: dbExample
+        example: dbExamples
       });
     });
   });
