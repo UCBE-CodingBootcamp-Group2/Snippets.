a1245ff716e7b6ac3becaf17408da83c064d9b32
Skarrg <orestes.moiran@gmail.com>
2019-01-19 11:15:29 -0800

    pls travis

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -74,7 +74,7 @@ module.exports = function(app) {
     // Load example page and pass in an example by id
     app.get("/example/:id", function(req, res) {
       db.Example.findOne({ where: { id: req.params.id } }).then(function(
-        dbExample
+        dbExamples
       ) {
         res.render("example", {
           example: dbExamples
