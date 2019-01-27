9ba6f03b9c5df30de7cc9589345b0eb071ee8a7f
cindy <42073246+cxlhyphen@users.noreply.github.com>
2019-01-27 00:16:18 -0500

    root page + main page edits

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,6 +20,14 @@ module.exports = function(app) {
     if (req.user) {
       res.redirect("/home");
     }
+    res.sendFile(path.join(__dirname, "../public/root.html"));
+  });
+
+  app.get("/signup", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/home");
+    }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
